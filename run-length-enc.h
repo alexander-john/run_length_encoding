49b6bc261e281de1bfce5c8c49348c7432df4f83
Alexander John <alexjohnpersonals@gmail.com>
2023-12-10 11:36:13 -0700

    put header file functions in the correct place (inside the preprocessor directive) :)

--- run-length-enc.h
+++ run-length-enc.h
@@ -5,8 +5,10 @@
 #ifndef RUN_LENGTH_ENCODING_RUN_LENGTH_ENC_H
 #define RUN_LENGTH_ENCODING_RUN_LENGTH_ENC_H
 
-#endif //RUN_LENGTH_ENCODING_RUN_LENGTH_ENC_H
+#include <string>
 
 std::string runLengthEncode(std::string str);
 
 std::string runLengthDecode(const std::string& str);
+
+#endif //RUN_LENGTH_ENCODING_RUN_LENGTH_ENC_H
